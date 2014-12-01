a409f9696bd921a8f3158c58fbf3ca09bc0858fb
rodrigodealexandre <rodrigodealexandre@hotmail.com>
2014-12-01 14:05:42 -0200

    Exploratory Data Analysis fixed

---
+++ Exploratory Data Analysis/Data/Assignment 1/plot4.R
@@ -0,0 +1,26 @@
+plot4 <- function() {
+    hc <- read.table("household_power_consumption.txt", sep=";",header=T,na.strings ="?")
+    hc<- na.omit(hc)
+    df <- within(hc, datetime <- as.POSIXlt(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"))
+    
+    sdf <- subset(df, as.Date(df$Date, "%d/%m/%Y") == "2007-02-01" | as.Date(df$Date, "%d/%m/%Y") == "2007-02-02")
+    
+    png(filename = "plot4.png", width = 480, height = 480)
+    
+    par(mfcol = c(2,2))
+    
+    plot(sdf$datetime, sdf$Global_active_power, type="l", ylab = "Global Active Power (KiloWatts)", xlab = "" )
+        
+    plot(sdf$datetime, sdf$Sub_metering_1, type="l", ylab = "Energy sub metering", xlab = "")
+    lines(sdf$datetime, as.numeric(as.character(sdf$Sub_metering_2)), type = "l", col = "red")
+    lines(sdf$datetime, as.numeric(as.character(sdf$Sub_metering_3)), type = "l", col = "blue")
+    legend("topright", pch = "_", col =  c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    
+    plot(sdf$datetime, sdf$Voltage, type="l", ylab = "Voltage", xlab = "datetime")
+    
+    plot(sdf$datetime, sdf$Global_reactive_power, type="l", ylab = "Global_reactive_power", xlab = "datetime")
+    
+        
+    dev.off()
+    
+}
